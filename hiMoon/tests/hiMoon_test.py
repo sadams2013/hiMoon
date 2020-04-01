d50672945d527ff7a87fe682cf10b0fec488f239
Solomon Adams <sadams07@su.edu>
2020-04-01 15:02:25 -0400

    updated and added unit tests

--- hiMoon/tests/hiMoon_test.py
+++ hiMoon/tests/hiMoon_test.py
@@ -1,23 +1,38 @@
 import unittest
 import csv
 
+import pandas as pd
+
 from hiMoon import gene, vcf, subject, config
 
-config = config.ConfigData("hiMoon/tests/default_config.ini")
+CONFIG = config.ConfigData("hiMoon/tests/config.ini")
+VCF = vcf.VarFile("hiMoon/tests/NA12878_chr22.bcf")
+CYP2D6_TABLE = "hiMoon/tests/CYP2D6.NC_000022.11.haplotypes.tsv"
+GENE = gene.Gene(CYP2D6_TABLE, config = CONFIG, vcf = VCF)
+SUBJ = subject.Subject("NA12878",genes = [GENE], config = CONFIG)
 
 class TestGene(unittest.TestCase):
 
     def test_gene(self):
-        self.gene_obj = gene.Gene("hiMoon/tests/CYP2D6.NC_000022.11haplotypes.tsv", config = config)
+        assert GENE.gene == "CYP2D6"
+    
+    def test_translation_table_version(self):
+        assert gene.Gene.get_version(CYP2D6_TABLE) == "pharmvar-4.1.4"
+    
+    def test_translation_table_read(self):
+        table = gene.Gene.read_translation_table(CYP2D6_TABLE, CONFIG)[0]
+        assert type(table) == pd.DataFrame
 
 
-class TestSubjet(unittest.TestCase):
+class TestSubject(unittest.TestCase):
 
-    def test_subject(self):
-        self.gene_obj = gene.Gene("hiMoon/tests/CYP2D6.NC_000022.11haplotypes.tsv", config = config)
-        subj = subject.Subject(
-            "test",
-            genes = [self.gene_obj],
-            config = config
-            )
+    def test_subject_prefix(self):
+        assert SUBJ.prefix == "NA12878"
+    
+    def test_called_haplotypes(self):
+        assert SUBJ.called_haplotypes["CYP2D6"][1] == [("CYP2D6(star)3", 1.0), ("CYP2D6(star)4.001", 1.0)]
 
+class TestVCF(unittest.TestCase):
+
+    def test_samples(self):
+        assert VCF.samples[0] == "NA12878"
