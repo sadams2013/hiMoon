e9ff4227a1e53b09f8992bdf4dff5e4c42222ecd
Solomon Adams <sadams2013@gmail.com>
2020-06-30 12:15:55 -0400

    MIT to Apache 2.0

--- hiMoon/__init__.py
+++ hiMoon/__init__.py
@@ -1,6 +1,20 @@
+#    Copyright 2020 Solomon M. Adams
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
 __author__ = "Solomon M. Adams, PharmD, PhD"
 __copyright__ = "Copyright 2020, Solomon M. Adams"
-__license__ = "MIT"
+__license__ = "Apache v2.0"
 __version__ = "0.1"
 __maintainer__ = "Solomon M. Adams, PharmD, PhD"
 __email__ = "sadams07@su.edu"
