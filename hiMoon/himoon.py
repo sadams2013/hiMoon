3248fc2fe557b2eb0f3ffe34feed7ea830384d4c
sadams2013 <sadams2013@gmail.com>
2020-07-09 14:47:21 -0400

    solver argument to api

--- hiMoon/himoon.py
+++ hiMoon/himoon.py
@@ -18,7 +18,7 @@ from .haplotype import Haplotype
 from .subject import Subject
 from . import CONFIG, set_config
 
-def get_haps_from_vcf(translation_table_path: str, vcf_file_path: str, sample_id: str, config_path = None) -> tuple:
+def get_haps_from_vcf(translation_table_path: str, vcf_file_path: str, sample_id: str, solver: str = "CBC", config_path: str = None) -> tuple:
     """
     Provide a VCF file, sample ID, and translation table
     Get called haplotypes and additional information
@@ -35,12 +35,12 @@ def get_haps_from_vcf(translation_table_path: str, vcf_file_path: str, sample_id
     if config_path:
         set_config(config_path)
     vcf = VarFile(vcf_file_path, sample_id)
-    gene = AbstractGene(translation_table_path, vcf = vcf)
+    gene = AbstractGene(translation_table_path, vcf = vcf, solver = solver)
     haplotype = Haplotype(gene, sample_id)
     haplotype.table_matcher()
     return haplotype.optimize_hap()
 
-def get_haps_from_variants(translation_table_path: str, vcf_data: str, sample_id: str, config_path = None) -> tuple:
+def get_haps_from_variants(translation_table_path: str, vcf_data: str, sample_id: str, solver: str = "CBC", config_path: str = None) -> tuple:
     """
     Same as get_haps_from_vcf, but bypasses the VCF file so that you can provide formatted variants from another input
     Get called haplotypes and additional information
@@ -56,7 +56,7 @@ def get_haps_from_variants(translation_table_path: str, vcf_data: str, sample_id
     """
     if config_path:
         set_config(config_path)
-    gene = AbstractGene(translation_table_path, variants = vcf_data)
+    gene = AbstractGene(translation_table_path, variants = vcf_data, solver = solver)
     haplotype = Haplotype(gene, sample_id)
     haplotype.table_matcher()
     return haplotype.optimize_hap()
