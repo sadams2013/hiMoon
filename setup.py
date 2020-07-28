624bb3d98203ef1fc057d63ae645b3d62a64c5ee
sadams2013 <sadams2013@gmail.com>
2020-07-28 12:29:02 -0400

    increment to 0.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ base = None
 
 setup(
     name="hiMoon",
-    version="0.7",
+    version="0.8",
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
