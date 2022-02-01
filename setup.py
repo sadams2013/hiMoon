8f91a39d91eae763c8fd2668bb0bf2da5317073b
Solomon Adams <sadams2013@gmail.com>
2022-01-31 23:28:30 -0500

    0.13.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ base = None
 
 setup(
     name="hiMoon",
-    version="0.13.0",
+    version="0.13.1",
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
